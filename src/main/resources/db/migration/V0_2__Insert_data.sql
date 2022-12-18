233b441120b16702826bd026c7abbb295bee45f1
Judicael <99041636+JrManjato@users.noreply.github.com>
2022-12-19 02:47:41 +0300

    feat: initial commit

---
+++ src/main/resources/db/migration/V0_2__Insert_data.sql
@@ -0,0 +1,17 @@
+insert into team("name")
+values ('Paris Saint-Germain'),
+       ('FC Barcelone');
+insert into player(name, number, id_team)
+VALUES ('Neymar JR', 10, 1),
+       ('Ansu Fati', 22, 2);
+insert into sponsor(name)
+VALUES ('Qatar Airways'),
+       ('Spotify');
+
+insert into play_against (datetime, stadium, id_home, id_opponent)
+values (current_timestamp, 'Camp Nou', 1, 2);
+
+insert into have_sponsor(id_team, id_sponsor)
+VALUES (1, 1),
+       (1, 2),
+       (2, 1);
