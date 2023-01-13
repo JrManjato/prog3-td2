2e44487d2f58e412924f37e1d05d95ab7b01fbbb
Judicael <99041636+JrManjato@users.noreply.github.com>
2023-01-13 11:53:28 +0300

    fix: update the specification

--- src/main/resources/db/migration/V0_2__Insert_data.sql
+++ src/main/resources/db/migration/V0_2__Insert_data.sql
@@ -1,5 +1,6 @@
 insert into team("name")
 values ('Paris Saint-Germain'),
+       ('Monaco'),
        ('FC Barcelone');
 insert into player(name, number, id_team)
 VALUES ('Gianluigi Donnarumma', 99, 1),
@@ -10,7 +11,8 @@ VALUES ('Qatar Airways'),
        ('Spotify');
 
 insert into play_against (datetime, stadium, id_home, id_opponent)
-values (current_timestamp, 'Camp Nou', 1, 2);
+values (current_timestamp, 'Camp Nou', 1, 2),
+       (current_timestamp, 'Parc des Princes', 1, 3);
 
 insert into have_sponsor(id_team, id_sponsor)
 VALUES (1, 1),
